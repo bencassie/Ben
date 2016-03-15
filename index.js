dd9d01d5f4783b36516c6a0a4a45eb616cc38d53
Ben Cassie <Ben Cassie>
2016-03-15 15:39:31 +0000

    1st

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var app = express();
 
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use('/public', express.static('public'));
+
+var cats = require('./cat.js')(app);
+
+
+mongoose.connect('mongodb://localhost/cats');
+
 var server = app.listen(3000, function() {
 	console.log('Server running');
 });
 
-app.use('/public', express.static('public'));
 
 app.get('/hello', function (req, res) {
-	//res.send('Hello world!');
-	res.json({'hello':'wurzel'});
+	res.json({'hello':'ben'});
 });
