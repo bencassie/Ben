4edb053875ac046611e53f212855edc21e71e033
Ben Cassie <Ben Cassie>
2016-03-18 00:43:59 +0000

    cors put

--- public/js/js.js
+++ public/js/js.js
@@ -8,8 +8,21 @@
             url:'http://ben.db.com/report/5G/2016-03-16',
             dataType: 'json',
             success: function(data, status, xhr) {
-                $('#outputDiv').text(JSON.stringify(data) + 'Name = ' + data.report.name);
-                //$('#outputDiv').text(data);
+                var output = JSON.stringify(data) + 'Name = ' + data.report.name;
+                $('#outputDiv').text(output);
+                // $.ajax({
+                //     url: 'http://ben.db.com/report/' + data.report._id,
+                //     dataType: 'json',
+                //     type: 'put',
+                //     data: {"state":"review"},                    
+                //     success: function(data, status, xhr) {
+                //         $('#outputDiv').text(output + JSON.stringify(data) + 'Name = ' + data.report.name);
+                //         //$('#outputDiv').text(data);
+                //     },
+                //     error: function(xhr, status, error) {
+                //         $('#outputDiv').text('error occured: ' + status);
+                //     }
+                // }); 
             },
             error: function(xhr, status, error) {
                 $('#outputDiv').text('error occured: ' + status);
