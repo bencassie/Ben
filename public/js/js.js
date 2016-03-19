f0349195c948026a58dd18d2751f8cf3aff4974e
Ben Cassie <Ben Cassie>
2016-03-19 19:52:19 +0000

    painting buttons

--- public/js/js.js
+++ public/js/js.js
@@ -1,15 +1,11 @@
  $(function() {
-        Init();
-    });
-    
-    function Init()
-    {
         $.ajax({
             url:'http://ben.db.com/report/5G/2016-03-16',
             dataType: 'json',
             success: function(data, status, xhr) {
-                var output = JSON.stringify(data) + 'Name = ' + data.report.name;
-                $('#outputDiv').text(output);
+                refreshWorkflow(data)
+                // var output = JSON.stringify(data) + 'Name = ' + data.report.name;
+                // $('#outputDiv').text(output);
                 // $.ajax({
                 //     url: 'http://ben.db.com/report/' + data.report._id,
                 //     dataType: 'json',
@@ -30,6 +26,16 @@
         }); 
         
         var a = 1;           
+    });
+    
+    refreshWorkflow = function(data) {
+        data.report.actions.forEach(function(action) {
+           $("#workflow ul").append("<span class='workflowAction' id='action" + action + "' action='" + action + "'><li><a>" + action + "</a></li></span>");
+        });
+        
+        $('.workflowAction').on('click', function(e,a) {
+            alert(this.attributes.action.value);
+        });
     }
     // var b = $.parseJSON('{"name":"ben"}');
     // undefined
--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -2,5 +2,6 @@
 /// <reference path="browser/ambient/cors/index.d.ts" />
 /// <reference path="browser/ambient/express-serve-static-core/index.d.ts" />
 /// <reference path="browser/ambient/express/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
 /// <reference path="browser/ambient/mongoose/index.d.ts" />
 /// <reference path="browser/ambient/serve-static/index.d.ts" />
